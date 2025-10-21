2b8839c23dfb365486a5c2879bd40534b7a222cc
Dan Molache <skyarym4ik@gmail.com>
2025-10-21 13:09:59 +0300

    hobbies, address, stateCity input logic

--- src/test/java/org/example/tests/FormTest.java
+++ src/test/java/org/example/tests/FormTest.java
@@ -17,6 +17,11 @@ public class FormTest {
     public static final String USER_MOBILE_NUMBER = "0612345678";
     public static final String USER_BIRTH_DATE = "15 Mar 2006";
     public static final String[] USER_SUBJECTS = {"Hindi", "Social Studies", "Physics", "Maths"};
+    public static final String[] USER_HOBBIES = {"Sports", "Music"};
+//    public static final String USER_PICTURE = "";
+    public static final String USER_STATE = "Rajasthan";
+    public static final String USER_CITY = "Jaiselmer";
+    public static final String USER_ADDRESS = "VIVAN PALACE, AND, near HOTEL, near PETROL PUMP, Gandhi Colony, Gandhi Nagar, Jaiselmer, Rajasthan, India";
 
     @BeforeClass
     public void beforeTest() {
@@ -27,7 +32,9 @@ public class FormTest {
     @Test
     public void formTest() {
         driver.get(URL);
+
         FormPom form = new FormPom(driver);
+
         form.setFirstName(FIRST_NAME);
         form.setLastName(LAST_NAME);
         form.setUserEmail(USER_EMAIL);
@@ -36,6 +43,13 @@ public class FormTest {
         form.setUserMobileNumber(USER_MOBILE_NUMBER);
         form.setUserBirthDate(USER_BIRTH_DATE);
         form.setUserSubjects(USER_SUBJECTS);
+        form.setUserHobbies(USER_HOBBIES);
+//        form.setUserPicture(USER_PICTURE);
+        form.setUserAddress(USER_ADDRESS);
+        form.setUserState(USER_STATE);
+        form.setUserCity(USER_CITY);
+
+        form.submitForm();
     }
 
     @AfterClass

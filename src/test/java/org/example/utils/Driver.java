56a8e1cba75f678e96c7b610892c05285ccfee3f
Dan Molache <skyarym4ik@gmail.com>
2025-11-19 13:07:10 +0200

    git actions

--- src/test/java/org/example/utils/Driver.java
+++ src/test/java/org/example/utils/Driver.java
@@ -1,8 +1,8 @@
 package org.example.utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -12,9 +12,26 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Driver {
+
     static public WebDriver getAutoLocalDriver() {
         WebDriverManager.edgedriver().setup();
-        return new EdgeDriver();
+
+        EdgeOptions options = new EdgeOptions();
+
+        if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--remote-debugging-port=9222");
+            options.addArguments("--disable-extensions");
+            options.addArguments("--disable-infobars");
+        }
+
+        options.addArguments("--remote-allow-origins=*");
+
+        return new EdgeDriver(options);
     }
 
     static public WebDriver getLocalDriver() {
@@ -28,30 +45,23 @@ public class Driver {
         EdgeOptions options = new EdgeOptions();
         options.setCapability("browserVersion", "128.0");
         options.setCapability("selenoid:options", new HashMap<String, Object>() {{
-            /* How to add test badge */
             put("name", "Test badge...");
-
-            /* How to set session timeout */
             put("sessionTimeout", "15m");
-
-            /* How to set timezone */
             put("env", new ArrayList<String>() {{
                 add("TZ=UTC");
             }});
-
-            /* How to enable video recording */
             put("enableVideo", true);
             put("enableVNC", true);
             put("enableLog", true);
             put("noSandbox", true);
             put("headless", true);
         }});
-        RemoteWebDriver remoteDriver = null;
+
         try {
-            remoteDriver = new RemoteWebDriver(URI.create("http://127.0.0.1:4444/wd/hub").toURL(), options);
+            return new RemoteWebDriver(URI.create("http://127.0.0.1:4444/wd/hub").toURL(), options);
         } catch (MalformedURLException e) {
             e.printStackTrace();
+            return null;
         }
-        return remoteDriver;
     }
 }
